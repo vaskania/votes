5ea088203c2fdd9776b7c8cd48f89f3c4e40dae0
vaskania <vaskania26@gmail.com>
2021-12-29 14:15:35 +0400

    changed basic to jwt in swagger, added logger and try/cath to auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,18 +1,24 @@
 const jwt = require('jsonwebtoken');
+const logger = require('../log/logger');
 
 const verifyToken = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  const token = authHeader && authHeader.split(' ')[1];
+  try {
+    const authHeader = req.headers.authorization;
+    const token = authHeader && authHeader.split(' ')[1];
 
-  if (token == null) return res.sendStatus(401);
+    if (token == null) return res.sendStatus(401);
 
-  jwt.verify(token, process.env.SECRET, (err, user) => {
-    if (err) {
-      return res.status(401).send({ error: { message: 'Invalid token' } });
-    }
-    req.user = user;
-    next();
-  });
+    jwt.verify(token, process.env.SECRET, (err, user) => {
+      if (err) {
+        logger.error(err);
+        return res.status(401).send({ error: { message: 'Invalid token' } });
+      }
+      req.user = user;
+      next();
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = verifyToken;
